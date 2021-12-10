1f73a8236fd78918e285efb4e9f7a3a3c8dade52
최주은 <ilutoo@naver.com>
2021-12-10 13:54:35 +0900

    [Update] PreferenceUtil.kt 작성, 회원 정보 기입, 반영, 회원 UI 작성, 정보 연동, 피드백 반영 --- 최주은

--- app/src/main/java/com/ssafy/ssaign/src/main/MainActivity.kt
+++ app/src/main/java/com/ssafy/ssaign/src/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ssafy.ssaign.src.main
 
 import android.os.Bundle
+import android.util.Log
 import com.ssafy.ssaign.config.BaseActivity
 import com.ssafy.ssaign.R
+import com.ssafy.ssaign.config.PreferenceUtil
 import com.ssafy.ssaign.databinding.ActivityMainBinding
 import com.ssafy.ssaign.src.main.report.MakeReportFragment
 import com.ssafy.ssaign.src.main.sign.SignFragment
@@ -16,19 +18,30 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
 
         //sharedpreferences를 사용하여 정보를 가져온다.
+        prefs = PreferenceUtil(this)
+        val user = prefs.getUser()
+        Log.d("SSAIGN-MainActivity", user.toString())
 
         //정보가 있다면 -> Viewmodel에 저장 -> MakeReportFramgent -> Report 생성
+        if(user != null){
+            if(currentFragment == null){
+                val fragment = MakeReportFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit()
+            }
+        }
 
         //정보가 없다면 -> UserInfoFragment -> 정보 기입 -> Viewmodel에 저장 -> MakeReportFramgnet -> Report 생성
-
-        //임시로 한 것
-
-        if(currentFragment == null){
-            val fragment = SignFragment()
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragment_container, fragment)
-                .commit()
+        else{
+            if(currentFragment == null){
+                val fragment = UserInfoFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit()
+            }
         }
     }
 
@@ -55,5 +68,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
                 .addToBackStack(null)
                 .commit()
         }
+    }
+
+    companion object {
+        lateinit var prefs: PreferenceUtil
     }
 }
